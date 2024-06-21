a1da1bba923e7f143f851e2691eefb8b63fa9bd0
chaserileyroberts <chaserileyroberts@gmail.com>
2024-06-20 17:51:40 -0700

    float around and find out AI

--- crates/sipho/src/objects/ai.rs
+++ crates/sipho/src/objects/ai.rs
@@ -18,9 +18,19 @@ impl Plugin for EnemyAIPlugin {
     }
 }
 
-#[derive(Component, Debug, Reflect, Default)]
+#[derive(Component, Debug, Reflect)]
 pub struct EnemyAI {
     free_workers: HashSet<Entity>,
+    clear_objectives_timer: Timer,
+}
+
+impl Default for EnemyAI {
+    fn default() -> EnemyAI {
+        EnemyAI {
+            free_workers: HashSet::new(),
+            clear_objectives_timer: Timer::from_seconds(0.1, TimerMode::Repeating),
+        }
+    }
 }
 
 // Runs BFS to find the last entity of the shortest limb.
@@ -63,19 +73,31 @@ impl EnemyAI {
         mut commands: ObjectCommands,
         mut elastic_events: EventWriter<SpawnElasticEvent>,
         mut audio: EventWriter<AudioEvent>,
+        time: Res<Time>,
     ) {
         for (mut head, head_entity, team, mut ai) in query.iter_mut() {
+            let leaves = get_all_leaves(head_entity, &attached_to);
+            ai.clear_objectives_timer.tick(time.delta());
+            if ai.clear_objectives_timer.finished() {
+                for leaf in ai.free_workers.iter().chain(leaves.iter()) {
+                    let mut objective = objective_query.get_mut(*leaf).unwrap();
+                    objective.clear();
+                    objective.push(Objective::Idle);
+                }
+                ai.clear_objectives_timer.reset();
+            }
             ai.free_workers.retain(|x| positions.contains(*x));
             // Apply free worker useful objectives
             let useful_objective = ai
                 .free_workers
                 .iter()
+                .chain(leaves.iter())
                 .map(|x| objective_query.get(*x).unwrap())
                 .find(|x| !matches!(x.last(), Objective::Idle));
             // Force all of the arms to have useful objects if it can.
             if let Some(useful_objective) = useful_objective {
                 let useful_objective = useful_objective.clone();
-                for leaf in ai.free_workers.iter() {
+                for leaf in ai.free_workers.iter().chain(leaves.iter()) {
                     let mut objective = objective_query.get_mut(*leaf).unwrap();
                     if objective.last() == &Objective::Idle {
                         objective.pop();
@@ -84,25 +106,7 @@ impl EnemyAI {
                 }
             }
 
-            let leaves = get_all_leaves(head_entity, &attached_to);
-            let useful_objective = leaves
-                .iter()
-                .chain(ai.free_workers.iter())
-                .map(|x| objective_query.get(*x).unwrap())
-                .find(|x| !matches!(x.last(), Objective::Idle));
-            // Force all of the arms to have useful objects if it can.
-            if let Some(useful_objective) = useful_objective {
-                let useful_objective = useful_objective.clone();
-                for leaf in leaves.iter() {
-                    let mut objective = objective_query.get_mut(*leaf).unwrap();
-                    if objective.last() == &Objective::Idle {
-                        objective.pop();
-                        objective.push(useful_objective.last().clone())
-                    }
-                }
-            }
             let (entity, arm_length) = head.get_next_limb(head_entity, &attached_to);
-
             let position = positions.get(entity).unwrap();
             if commands.try_consume(head_entity, 1).is_ok() {
                 let direction = Vec2::Y;
@@ -118,6 +122,10 @@ impl EnemyAI {
                         &mut commands,
                         entity,
                     );
+                    if let Ok(mut objective) = objective_query.get_mut(entity) {
+                        objective.clear();
+                        objective.push(Objective::Idle);
+                    }
                 } else if commands.try_consume(head_entity, 3).is_ok() {
                     let position = positions.get(head_entity).unwrap();
                     if let Some(new_entity) = commands.spawn(ObjectSpec {
